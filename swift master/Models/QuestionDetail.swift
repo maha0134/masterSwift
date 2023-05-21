ef43921688a9f7f47a4987f01291a6e75bc9f407
Akshay Mahajan <maha0134@algonquinlive.com>
2023-05-21 12:04:35 -0400

    add question model and sample json data

---
+++ swift master/Models/QuestionDetail.swift
@@ -0,0 +1,28 @@
+//
+//  QuestionDetail.swift
+//  swift master
+//
+//  Created by AKSHAY MAHAJAN on 2023-05-21.
+//
+
+import Foundation
+
+struct QuestionBank: Codable {
+	var questions: [Question]
+}
+
+struct Question: Codable {
+	var question: String
+	var choices: [String]
+	var correctChoice: Int
+	var difficulty: Int
+	
+	enum CodingKeys: String, CodingKey {
+		case question
+		case choices
+		case correctChoice = "correct_choice"
+		case difficulty
+	}
+}
+
+
