b4c6474ce2e093c8caaa53419c0e13d98d65e221
Akshay Mahajan <maha0134@algonquinlive.com>
2023-05-21 11:29:42 -0400

    Initial Commit

---
+++ swift master/swift_masterApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_masterApp.swift
+//  swift master
+//
+//  Created by AKSHAY MAHAJAN on 2023-05-21.
+//
+
+import SwiftUI
+
+@main
+struct swift_masterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
